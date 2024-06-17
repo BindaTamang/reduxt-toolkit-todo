2ea11e84e90181c30500f0c2c95923b9340b5a95
unknown <bindatmg5@gmail.com>
2024-06-17 12:39:28 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+import { Provider } from 'react-redux';
+import {store} from './App/store'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center p-4">
+        <h1 className="text-4xl font-bold mt-8">Todo List Redux Toolkit</h1>
+        <AddTodo />
+        <Todos />
+      </div>
+    </Provider>
   );
 }
 
